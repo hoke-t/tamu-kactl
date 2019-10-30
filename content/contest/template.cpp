b30257928eb9505436dc1c5ec6d330a780aaf46b
Tanner Hoke <hoke-t@users.noreply.github.com>
2019-10-30 18:00:12 -0500

    TAMU flair

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
 #define trav(a, x) for(auto& a : x)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define pb push_back
+#define fst first
+#define snd second
+
 typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+typedef long double ld;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
 
 int main() {
-	cin.sync_with_stdio(0); cin.tie(0);
-	cin.exceptions(cin.failbit);
+  cin.sync_with_stdio(0); cin.tie(0);
+  cin.exceptions(cin.failbit);
 }
