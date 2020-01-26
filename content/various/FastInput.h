9b6d30f00906255d19015b9b27f56d0f9b7c4e7d
Tanner Hoke <tanner_h@me.com>
2020-01-26 17:44:50 -0600

    pull fast io from upstream

--- content/various/FastInput.h
+++ content/various/FastInput.h
@@ -12,21 +12,21 @@
 #pragma once
 
 struct GC {
-	char buf[1 << 16];
-	int bc = 0, be = 0;
-	char operator()() {
-		if (bc >= be) {
-			buf[0] = bc = 0;
-			be = fread(buf, 1, sizeof(buf), stdin);
-		}
-		return buf[bc++]; // returns 0 on EOF
-	}
+  char buf[1 << 16];
+  int bc = 0, be = 0;
+  char operator()() {
+    if (bc >= be) {
+      buf[0] = bc = 0;
+      be = fread(buf, 1, sizeof(buf), stdin);
+    }
+    return buf[bc++]; // returns 0 on EOF
+  }
 } gc;
 int read_int() {
-	char c;
-	while ((c = gc()) < 40);
-	if (c == '-') return -read_int();
-	int a = c - '0';
-	while (isdigit(c = gc())) a = a * 10 + c -'0';
-	return a;
+  char c;
+  while ((c = gc()) < 40);
+  if (c == '-') return -read_int();
+  int a = c - '0';
+  while (isdigit(c = gc())) a = a * 10 + c -'0';
+  return a;
 }
