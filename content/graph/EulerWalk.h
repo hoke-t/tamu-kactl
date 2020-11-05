2af622483593112c0d3d8730265852300ef61f77
Tanner Hoke <tanner_h@me.com>
2020-11-05 14:11:05 -0600

    idk

--- content/graph/EulerWalk.h
+++ content/graph/EulerWalk.h
@@ -1,5 +1,5 @@
 /**
- * Author: Simon Lindholm
+ * Author: Simon Lindholm, Tanner Hoke
  * Date: 2019-12-31
  * License: CC0
  * Source: folklore
@@ -7,25 +7,27 @@
  * Input should be a vector of (dest, global edge index), where
  * for undirected graphs, forward/backward edges have the same index.
  * Returns a list of nodes in the Eulerian path/cycle with src at both start and end, or
- * empty list if no cycle/path exists.
- * To get edge indices back, add .second to s and ret.
+ * empty list if no cycle/path exists. The returned vector is of (node, global edge index to node).
+ * The second element of the first pair is always $-1$.
  * Time: O(V + E)
  * Status: stress-tested
  */
 #pragma once
 
-vi eulerWalk(vector<vector<pii>>& gr, int nedges, int src=0) {
+vii eulerWalk(vector<vector<pii>>& gr, int nedges, int src=0) {
   int n = sz(gr);
-  vi D(n), its(n), eu(nedges), ret, s = {src};
-  D[src]++; // to allow Euler paths, not just cycles
+  vi D(n), its(n), eu(nedges);
+  vii ret, s = {make_pair(src, -1)};
+  D[src]++; // comment this line to disallow Euler paths, only allow cycles
   while (!s.empty()) {
-    int x = s.back(), y, e, &it = its[x], end = sz(gr[x]);
-    if (it == end){ ret.push_back(x); s.pop_back(); continue; }
+    int x = s.back().fst, y, e, &it = its[x], end = sz(gr[x]);
+    if (it == end){ ret.emplace_back(x, s.back().snd); s.pop_back(); continue; }
     tie(y, e) = gr[x][it++];
     if (!eu[e]) {
       D[x]--, D[y]++;
-      eu[e] = 1; s.push_back(y);
+      eu[e] = 1; s.emplace_back(y, e);
     }}
   for (int x : D) if (x < 0 || sz(ret) != nedges+1) return {};
   return {ret.rbegin(), ret.rend()};
 }
+
