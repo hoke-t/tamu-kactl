86caaf704646bec5e731a7f39b113fb7b7fdfca1
Tanner Hoke <tanner_h@me.com>
2020-11-06 01:27:36 -0600

    FIX

--- stress-tests/utilities/template.h
+++ stress-tests/utilities/template.h
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, from, to) for (int i = from; i < (to); ++i)
-#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define per(i, a, b) for(int i = (b)-1; i >= (a); --i)
+#define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
 typedef long long ll;
+typedef long double ld;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pii> vii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
